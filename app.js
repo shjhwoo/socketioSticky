c85392c5e7cf841f7085489126660ba1dd7cf424
shjhwoo <108392916+shjhwoo@users.noreply.github.com>
2023-04-19 17:29:01 +0900

    최종

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 let port = 3000;
 
 process.argv.forEach((val, idx) => {
@@ -8,10 +12,25 @@ process.argv.forEach((val, idx) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!" + `from ${port}`);
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("message", (data) => {
+    console.log(`Message received: ${data}`);
+    socket.broadcast.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
-app.listen(port, () => {
+app.use(express.static("front"));
+
+app.get("/head", (req, res) => {
+  res.json(`여기 포트는: ${port}`);
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
